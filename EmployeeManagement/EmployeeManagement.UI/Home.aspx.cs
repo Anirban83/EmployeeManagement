5072685b8549e23deaad23c4cefef765f3a75c2b
Anirban83 <ani.chakraborty83@gmail.com>
2019-09-23 22:07:46 +0530

    Anonymous Authentication Implemented with Impersonation

---
+++ EmployeeManagement/EmployeeManagement.UI/Home.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace EmployeeManagement.UI
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("Application code executed using: ");
+            Response.Write(WindowsIdentity.GetCurrent().Name + "<br/>");
+
+            Response.Write("Is User Authenticated: ");
+            Response.Write(User.Identity.IsAuthenticated + "<br/>");
+
+            Response.Write("Authentication type: ");
+            Response.Write(User.Identity.AuthenticationType + "<br/>");
+
+            Response.Write("User Name: ");
+            Response.Write(User.Identity.Name + "<br/>");
+        }
+    }
+}
